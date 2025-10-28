111c5418d3ca58fcce3d8f7225a3fd610a57f7ad
2442903 <lditoro99@gmail.com>
2025-10-28 17:02:42 +0000

    Update Project.py
    Partial implementation of SSH, addition of dedicated CMD function and user input sanitisation functions.

--- Project.py
+++ Project.py
@@ -5,36 +5,61 @@ import subprocess
 password = "Train1ng$"
 user = "sysadmin"
 host = "192.168.1.10"
-cmd = ""
+remote = f"echo {password} | ssh {user}@{host}"
+
+def userInput(msg):
+
+in = input(msg)
+return in
+
+def exeCLICommand(cmd):
+
+"""
+
+Execute a CLI command and pipe the results to the Command line interface.
+
+Parameters:
+
+- cmd (str): The CLI command to execute.
+
+"""
+try:
+
+# execute the command and pipe the results to the CLI
+
+    subprocess.Popen(f"{cmd}", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+
+except Exception as e:
+
+# if an exception occurs, print the exception message as an error
+
+print("An error has occurred:\n", str(e))
 
 def remoteHomeDir():
-    cmd = "ls ~"
-    subprocess.Popen(f"echo {password} | ssh {user}@{host} {cmd}", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+    CMD = remote + " ls ~"
+    exeCLICommand()
 
 def remoteBackup(file):
-    cmd = f"cp -v {file} {file}.old"
-    subprocess.Popen(f"echo {password} | ssh {user}@{host} {cmd}", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+    CMD = remote + f"cp -v {file} {file}.old"
+    exeCLICommand()
 
 def copyURLDocs(URL):
-    print("")
+    cmd = f""
 
 quit = False
 while quit == False:
-    userInput = input("Please choose an option: \n").upper()
-    match userInput:
+
+    match input("Please choose an option: \n").upper():
         case "1":
             print(datetime.datetime.now())
         case "2":
             print(socket.gethostbyname(socket.gethostname()))
         case "3":
             remoteHomeDir()
-        case "4":
-            userInput = input("Please input the file path: \n")
-            remoteBackup(userInput)
-        case "5":
-            userInput = input("Please provide a valid URL: \n")
-
-            copyURLDocs(userInput)
+        case "4":
+            remoteBackup(userInput("Please input the file path: \n"))
+        case "5":
+            copyURLDocs(userInput("Please provide a valid URL: \n"))
         case "Q":
             quit = True
             exit
