ba959344f58910d64e0f300f330be71c34bd313d
2442903 <lditoro99@gmail.com>
2025-10-29 19:31:04 +0000

    Increase clairty and formatting

--- Project.py
+++ Project.py
@@ -7,69 +7,151 @@ user = "sysadmin"
 host = "192.168.1.10"
 remote = f"echo {password} | ssh {user}@{host}"
 
-def userInput(msg):
+def cleanUserInput(msg):
 
-in = input(msg)
-return in
+    """
+
+    Sanitize user input before returning the data.
+
+    Parameters:
+
+    - msg (str): user generated string to be sanitized.
+
+    """
+
+    data = input(msg)
+
+    return data
 
 def exeCLICommand(cmd):
 
-"""
+    """
 
-Execute a CLI command and pipe the results to the Command line interface.
+    Execute a CLI command and pipe the results to the Command line interface.
 
-Parameters:
+    Parameters:
 
-- cmd (str): The CLI command to execute.
+    - cmd (str): The CLI command to execute.
 
-"""
-try:
+    """
 
-# execute the command and pipe the results to the CLI
+    try:
 
-    subprocess.Popen(f"{cmd}", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+        # execute the command and pipe the results to the CLI
 
-except Exception as e:
+        subprocess.Popen(f"{cmd}", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
 
-# if an exception occurs, print the exception message as an error
+    except Exception as e:
 
-print("An error has occurred:\n", str(e))
+        # if an exception occurs, print the exception message as an error
+
+        print("An error has occurred:\n", str(e))
 
 def remoteHomeDir():
-    CMD = remote + " ls ~"
-    exeCLICommand()
+
+    """
+
+    Perform the "ls ~" in a remote terminal and pipe the results of the command to the local machine.
+
+    """
+
+    new_cmd = remote + " ls ~"
+    exeCLICommand(new_cmd)
 
 def remoteBackup(file):
-    CMD = remote + f"cp -v {file} {file}.old"
-    exeCLICommand()
 
-def copyURLDocs(URL):
-    cmd = f""
+    """
+
+    Perform a simple "cp" command on a remote machine and append the ".old" suffix to the copied file.
+
+    Parameters:
+
+    - file (str): The location and name of a remote file. 
+
+    """
+
+    new_cmd = remote + f"cp -v {file} {file}.old"
+    exeCLICommand(new_cmd)
+
+def copyURLDocs(url):
+
+    """
+
+    Download the web documents of the user given url.
+
+    Parameters:
+
+    - url (str): The target webpage which the user has entered to scrape.
+
+    """
+
+    new_cmd = f""
+    exeCLICommand(new_cmd)
 
 quit = False
 while quit == False:
 
-    match input("Please choose an option: \n").upper():
+    """
+
+    Simple while loop to maintain a CLI menu 
+
+    Runs indefinately until the user enters "q" or "Q" into the terminal
+
+    """
+
+    match cleanUserInput("Please choose an option: \n").upper():
+
         case "1":
+
+            # Print the local date and time of the host machine. 
+            # Format: yyyy-mm-dd hh:mm:ss.{milliseconds}
+
             print(datetime.datetime.now())
+
         case "2":
+
+            # Get the private ip address by way of getting the host socket information.
+            # Potential faliure case: if there is no internet connection expect the return of the loopback address instead.
+
             print(socket.gethostbyname(socket.gethostname()))
+
         case "3":
+
+            # Perform the "ls ~" in a remote terminal and pipe the results of the command to the local machine.
+
             remoteHomeDir()
-        case "4":
-            remoteBackup(userInput("Please input the file path: \n"))
-        case "5":
-            copyURLDocs(userInput("Please provide a valid URL: \n"))
+
+        case "4":
+
+            # Perform a simple "cp" command on a remote machine and append the ".old" suffix to the copied file.
+
+            remoteBackup(cleanUserInput("Please input the file path: \n"))
+
+        case "5":
+
+            # Download the web documents of the user given url.
+
+            copyURLDocs(cleanUserInput("Please provide a valid URL: \n"))
+
         case "Q":
+
+            # Quit the menu and end the program.
+
             quit = True
             exit
-        case _:
-            print("Invalid option.")
-""" 
-Used code from
- https://stackoverflow.com/questions/3586106/perform-commands-over-ssh-with-python
- https://www.geeksforgeeks.org/python/display-hostname-ip-address-python/
 
-https://brightdata.com/blog/how-tos/wget-with-python
+        case _:
+
+            print("Invalid option.")
+
+""" 
+
+Used code from
+
+    https://stackoverflow.com/questions/3586106/perform-commands-over-ssh-with-python
+
+    https://www.geeksforgeeks.org/python/display-hostname-ip-address-python/
+
+    https://brightdata.com/blog/how-tos/wget-with-python
 
 """
